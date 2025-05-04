a8f913249f6fe1474aa964b2fb8dfcf9352016e1
dwinney <daniel.winney@gmail.com>
2025-05-04 13:40:09 -0700

    added fitter parameter processing to fit real parts and fix imaginary parts

--- physics/amplitudes/kaon.hpp
+++ physics/amplitudes/kaon.hpp
@@ -26,6 +26,7 @@
 
 #include "Math/IntegratorMultiDim.h"
 #include "Math/IntegrationTypes.h"
+#include "TMatrixD.h"
 
 // The amplitudes are named with respect to isospin projections of the decay particle
 // into three pions. The order matters in that the definitions of s, t, and u.
@@ -225,6 +226,112 @@ namespace iterateKT
             L_zzz = new_amplitude<KL_PizPizPiz>(xkin);
             current = p_ppm;
         };
+
+        // We only fit the real parts of the parameters while the imaginary parts are
+        // given by requiring Taylor invariants have vanishing imaginary parts
+        inline std::vector<complex> process_fitter_parameters(std::vector<complex> in_pars)
+        {
+            double eps = 1E-3, s0 = _kinematics->s0();
+
+            //------------------------------------------------------------------------
+            // First we fix the imaginary parts of the M's and N's (total 3π I=1)
+            isobar F0 = get_isobar(id::dI1_I1_S0);
+            isobar F1 = get_isobar(id::dI1_I1_P1);
+            isobar F2 = get_isobar(id::dI1_I1_S2);
+            std::array<isobar,3> F = {F0, F1, F2};
+
+            // Grab all the Taylor coefficients
+            // First index is isospin, second is basis function ID
+            std::array<std::array<complex,4>,3> A, B, C;
+            for (uint i = 0; i <= 2; i++)
+            {
+                for (uint n = 0; n <= 3; n++)
+                {
+                    A[i][n] = F[i]->basis_function(n, 0);
+                    B[i][n] = F[i]->basis_function_derivative(n, eps);
+                    C[i][n] = F[i]->basis_function_second_derivative(n, eps)/2.;
+                };
+            };
+
+            // Construct the 4x4 matrix of Taylor invariants
+            // First index is which invariant, and second is basis_id
+            TArrayD reT_data(16), imT_data(16);
+            for (uint n = 0; n <= 3; n++)
+            {
+                // See first rows of Eq. 6.5 in [1]
+                std::array<complex,4> T;
+                T[0] = A[0][n] + s0*B[0][n] + 4*(A[2][n]+s0*B[2][n])/3;
+                T[1] = 3*A[1][n] + B[0][n] - 5*B[2][n]/3 + 9*s0*(B[1][n] + 2*s0*C[1][n]);
+                T[2] = 3*C[0][n] + 4*C[2][n];
+                T[3] = C[2][n] + B[1][n] + 3*s0*C[1][n];
+               
+                for (int j = 0; j <= 3; j++)
+                {
+                    reT_data[4*j+n] = real(T[j]);  imT_data[4*j+n] = imag(T[j]);
+                };
+            };
+            TMatrixD reT(4,4, reT_data.GetArray()), imT(4,4, imT_data.GetArray());
+
+            // Now we actually solve the matrix equation relating reMu and imMu
+            TMatrixD M = reT.Invert()*imT; M *= -1;
+
+            Double_t reMu_data[4], reNu_data[4];
+            for (int i = 0; i <= 3; i++)
+            { 
+                reMu_data[i] = real(in_pars[i]); reNu_data[i] = real(in_pars[i+5]);
+            };
+            TVectorD reMu(4, reMu_data), reNu(4, reNu_data);
+            TVectorD imMu = M*reMu, imNu = M*reNu;
+            
+            //------------------------------------------------------------------------
+            // Now do the same for the H's 
+
+            // These inhabit basis functions 9 & 10
+            std::array<isobar,3> H;
+            H[0] = nullptr;
+            H[1] = get_isobar(id::dI3_I2_P1); H[2] = get_isobar(id::dI3_I2_S2); 
+            for (uint i = 1; i <= 2; i++)
+            {
+                for (uint n = 0; n <= 1; n++)
+                {
+                    A[i][n] = H[i]->basis_function(n+9, 0);
+                    B[i][n] = H[i]->basis_function_derivative(n+9, eps);
+                    C[i][n] = H[i]->basis_function_second_derivative(n+9, eps)/2.;
+                };
+            };
+
+            // Construct the 4x4 matrix of Taylor invariants
+            // First index is which invariant, and second is basis_id
+            TArrayD reTp_data(4), imTp_data(4);
+            for (uint n = 0; n <= 1; n++)
+            {
+                // See second rows of Eq. 6.5 in [1]
+                std::array<complex,2> Tp;
+                Tp[0] = 3*A[1][n] - B[2][n] + 9*s0*(B[1][n] + 2*C[1][n]);
+                Tp[1] = 3*B[1][n] + C[2][n] + 9*s0*C[1][n];
+                               
+                for (int j = 0; j <= 1; j++)
+                {
+                    reTp_data[2*j+n] = real(Tp[j]);  imTp_data[2*j+n] = imag(Tp[j]);
+                };
+            };
+            TMatrixD reTp(2,2, reTp_data.GetArray()), imTp(2,2, imTp_data.GetArray());
+            TMatrixD Mp = reTp.Invert()*imTp; Mp *= -1;
+
+            Double_t reNup_data[2];
+            for (int i = 0; i <= 1; i++) reNup_data[i] = real(in_pars[i+9]);
+            TVectorD reNup(2, reNup_data), imNup = Mp*reNup;
+
+            //------------------------------------------------------------------------
+            // Finally repackaged and return
+            std::vector<complex> out_pars;
+            for (int i = 0; i <= 3; i++) out_pars.push_back(reMu[i] +I*imMu[i]);
+            out_pars.push_back(in_pars[4]); // Skip this one
+            for (int i = 0; i <= 3; i++) out_pars.push_back(reNu[i] +I*imNu[i]);
+            for (int i = 0; i <= 1; i++) out_pars.push_back(reNup[i]+I*imNup[i]);
+            if (out_pars.size() != in_pars.size())
+            return out_pars;
+        };
     
         inline complex prefactor_s(id iso_id, complex s, complex t, complex u)
         { return current->prefactor_s(iso_id, s, t, u); };
@@ -251,7 +358,7 @@ namespace iterateKT
         amplitude p_ppm, p_zzp, L_pmz, S_pmz, L_zzz;
     };
 
-    // Because the kaon mass is so small, the mass splittings between isospin projections
+    // Because the kaon mass is so smal, the mass splittings between isospin projections
     // make a noticable difference in the phase-space.
     // So, even though we calculate KT isobars in isospin limit, we can integrate with the realistic 
     double width_with_physical_masses(amplitude amp, double mK, std::array<double,3> m)
