278691bec076d999ac5d9335f8e1e6b84ac2cb14
dwinney <daniel.winney@nucleares.unam.mx>
2026-04-16 18:36:53 -0600

    nice m3pi intensity plots :)

--- scripts/pi1/plot_intensity_m3pi.cpp
+++ scripts/pi1/plot_intensity_m3pi.cpp
@@ -32,11 +32,8 @@ void plot_intensity_m3pi()
     int min = 11, max = 49; 
 
     // Which tbin to plot
-    uint tbin = 0;
-
-    // If we have two terms or three
-    bool minimal = false;
-
+    std::array<uint,2> tbins = {0, 3};
+    
     // Path to precalculated isoabrs
     std::string iso_path    = main_dir()+"/scripts/pi1/basis_functions/";
     // and the prefix given to each file
@@ -46,14 +43,32 @@ void plot_intensity_m3pi()
     // Data set up
 
     // Import our data sets  
-    std::vector<data_set> data;
-    for (int i = min; i <= max; i++) data.emplace_back(COMPASS::parse_JSON(i, tbin));
+    std::array<std::vector<data_set>,2> data;
+    for (int j = 0; j < 2; j++)
+    {
+       for (int i = min; i <= max; i++) data[j].emplace_back(COMPASS::parse_JSON(i, tbins[j]));
+    };
 
     // -----------------------------------------------------------------------
     // Import fit values
 
     std::vector<complex> minimal_pars    = COMPASS::import_parameters({min,max}, main_dir()+"/scripts/pi1/pars/minimal.dat");
-    std::vector<complex> nonminimal_pars = COMPASS::import_parameters({min,max}, main_dir()+"/scripts/pi1/pars/nonminimal.dat");
+    std::vector<complex> minimal_pars_c, minimal_pars_d;
+    for (int i = 0; i < (minimal_pars.size()-3)/3; i++)
+    {
+        minimal_pars_c.push_back( minimal_pars[3*i]   );
+        minimal_pars_c.push_back( 0. );
+        minimal_pars_c.push_back( 0. );
+        minimal_pars_d.push_back( 0. );
+        minimal_pars_d.push_back( 0. );
+        minimal_pars_d.push_back( minimal_pars[3*i+2]   );
+    };
+    for (int n = 0; n < 3; n++)
+    {
+        int m = minimal_pars.size()-3;
+        minimal_pars_c.push_back(minimal_pars[m+n]);
+        minimal_pars_d.push_back(minimal_pars[m+n]);
+    };
 
     // -----------------------------------------------------------------------
     // Set up amplitude and iterative solution
@@ -68,37 +83,59 @@ void plot_intensity_m3pi()
     // -----------------------------------------------------------------------
     // Set up amplitude and iterative solution
     
-    std::vector<double> mws, ews, nws, mws_c, nws_c;
-    for (auto bin : data)
+    std::array<std::vector<double>,2> mws, ews, mws_c, mws_d;
+    for (int j = 0; j < 2; j++)
     {
-        
-        double ew = 0, mw = 0, nw = 0;
-        for (int i = 0; i < bin._z.size(); i++) ew += norm(bin._z[i])*bin._dx[i];
-        ews.push_back(ew/1E4);
-        
-        COMPASS::fit_2D::process_parameters(minimal_pars, amp);
-        amp->set_option(option::set_tbin,         bin._extras["t_bin"]);
-        amp->set_option(option::set_mbin_COMPASS, bin._extras["m3pi_bin"]);
-        mws.push_back(amp->width()/1E4);
-        COMPASS::fit_2D::process_parameters(nonminimal_pars, amp);
-        amp->set_option(option::set_tbin,         bin._extras["t_bin"]);
-        amp->set_option(option::set_mbin_COMPASS, bin._extras["m3pi_bin"]);
-        nws.push_back(amp->width()/1E4);
+        for (auto bin : data[j])
+        {
+            
+            double ew = 0, mw = 0, nw = 0;
+            for (int i = 0; i < bin._z.size(); i++) ew += norm(bin._z[i])*bin._dx[i];
+            ews[j].push_back(ew/1E4);
+            
+            COMPASS::fit_2D::process_parameters(minimal_pars, amp);
+            amp->set_option(option::set_tbin,         bin._extras["t_bin"]);
+            amp->set_option(option::set_mbin_COMPASS, bin._extras["m3pi_bin"]);
+            mws[j].push_back(amp->width()/1E4);
+            COMPASS::fit_2D::process_parameters(minimal_pars_c, amp);
+            amp->set_option(option::set_tbin,         bin._extras["t_bin"]);
+            amp->set_option(option::set_mbin_COMPASS, bin._extras["m3pi_bin"]);
+            mws_c[j].push_back(amp->width()/1E4);
+            COMPASS::fit_2D::process_parameters(minimal_pars_d, amp);
+            amp->set_option(option::set_tbin,         bin._extras["t_bin"]);
+            amp->set_option(option::set_mbin_COMPASS, bin._extras["m3pi_bin"]);
+            mws_d[j].push_back(amp->width()/1E4);
+        };
     };
-
-    // -----------------------------------------------------------------------
-    // Plot results
+        
+        // -----------------------------------------------------------------------
+        // Plot results
 
     plotter plotter;
+
+    auto red   = square(jpacColor::Red,    "Contact Only");
+    red._draw_opt = "PL";
+    auto green = square(jpacColor::Green,  "Deck Only");
+    green._draw_opt = "PL";
 
     // Plot widths as a function of m3pi
     plot p1 = plotter.new_plot();
     p1.set_legend(0.65, 0.7);
-    p1.add_header("#minus #it{t} = " + to_string(-COMPASS::t_bins[tbin], 2) + " GeV^{2}");
-    p1.add_curve(m3pis, mws,  solid(jpacColor::Blue,  "Minimal"));
-    p1.add_curve(m3pis, nws,  solid(jpacColor::Red, "Non-minimal"));
-    p1.add_data (m3pis, ews,  dot(jpacColor::DarkGrey, "Data"));
-    p1.set_labels("#it{m}_{3#pi}  [GeV]", "#Gamma(#it{t}, #it{m}_{3#pi}^{2}) / 10^{4}  [a.u]");
-    p1.save("widths.pdf");
-   
+    p1.add_header("#minus #it{t} = " + to_string(-COMPASS::t_bins[0], 2) + " GeV^{2}");
+    p1.add_curve(m3pis, mws[0],    solid(jpacColor::Blue,   "Full")); 
+    p1.add_data (m3pis, ews[0],    dot(jpacColor::DarkGrey, "Data"));
+    p1.add_data(m3pis, mws_d[0],  green);
+    p1.add_data(m3pis, mws_c[0],  red);
+    p1.set_labels("#it{m}_{3#pi}  [GeV]", "#Gamma(#it{t}, #it{m}_{3#pi}^{2}) / 10^{4}  [a.u.]");
+
+    plot p2 = plotter.new_plot();
+    p2.set_legend(0.65, 0.7);
+    p2.add_header("#minus #it{t} = " + to_string(-COMPASS::t_bins[3], 2) + " GeV^{2}");
+    p2.add_curve(m3pis, mws[1],    solid(jpacColor::Blue,   "Full"));
+    p2.add_data (m3pis, ews[1],    dot(jpacColor::DarkGrey, "Data"));
+    p2.add_data(m3pis, mws_d[1],   green);
+    p2.add_data(m3pis, mws_c[1],   red);
+    p2.set_labels("#it{m}_{3#pi}  [GeV]", "#Gamma(#it{t}, #it{m}_{3#pi}^{2}) / 10^{4}  [a.u.]");
+
+    plotter.combine({2,1}, {p1, p2}, "intensity_m3pi.pdf");
 };
